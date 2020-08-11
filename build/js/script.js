ee33c62100ac6a1b2985815ff8c6e6669585b5c7
Ksenia Balashova <utyatka@gmail.com>
2020-08-11 21:27:51 +0200

    add color, time & date buttons

--- build/js/script.js
+++ build/js/script.js
@@ -19091,13 +19091,55 @@ $(function () {
   }
 
   const nameInput = mainForm.find(".main-form__name");
-  const buttons = mainForm.find(".main-form__color label");
+  const colorButtons = mainForm.find(".main-form__color label");
 
-  buttons.on("click", (evt) => {
+  colorButtons.on("click", (evt) => {
     const target = evt.target;
     const button = $(target);
     const color = button.css("background-color");
     nameInput.css("background-color", color);
+  });
+
+  const participantsInput = mainForm.find(".main-form__participants");
+  const participantsTooltip = mainForm.find(".main-form__part-input .tooltip");
+  const handleParticipantsInputUpdate = (evt) => {
+    let value;
+    const target = evt.target;
+    try {
+      value = parseInt(target.value, 10);
+    } catch (err) {}
+
+    if (value && value > 2) {
+      participantsTooltip.removeClass("tooltip--hidden");
+    } else {
+      participantsTooltip.addClass("tooltip--hidden");
+    }
+  };
+  participantsInput.on("change", handleParticipantsInputUpdate);
+  participantsInput.on("input", handleParticipantsInputUpdate);
+
+  const timeTemplate = mainForm.find("template.time")[0];
+  const addTime = (dateContainer) => {
+    const clon = timeTemplate.content.cloneNode(true);
+    dateContainer.appendChild(clon);
+  };
+  $(document).on("click", "button.main-form__addtime", (evt) => {
+    evt.preventDefault();
+    const target = evt.target;
+    const button = $(target);
+    const dateContainer = button.parent(".main-form__date")[0];
+    addTime(dateContainer);
+  });
+
+  const dateTemplate = mainForm.find("template.date")[0];
+  const datesContainer = mainForm.find(".main-form__dates")[0];
+  const addDate = () => {
+    const clon = dateTemplate.content.cloneNode(true);
+    datesContainer.appendChild(clon);
+  };
+  mainForm.on("click", "button.main-form__adddate", (evt) => {
+    evt.preventDefault();
+    addDate();
   });
 });
 
