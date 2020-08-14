b63d53afd827e3b46e9bf9a1d41640ad5c3bd798
Ksenia Balashova <utyatka@gmail.com>
2020-08-14 15:18:35 +0200

    fix

--- src/js/modules/input-jump.js
+++ src/js/modules/input-jump.js
@@ -1,11 +1,3 @@
-function testJump(x) {
-  var ml = ~~x.getAttribute("maxlength");
-  if (ml && x.value.length >= ml) {
-    do {
-      x = x.nextSibling;
-    } while (x && !/text/.test(x.type));
-    if (x && /text/.test(x.type)) {
-      x.focus();
-    }
-  }
-}
+$(function () {
+  $(".group-code").groupinputs();
+});
